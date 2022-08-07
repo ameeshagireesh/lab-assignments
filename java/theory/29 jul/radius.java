7acab621ad15c868ad040ac2007968eb85670334
Ameesha Gireesh <ameesha@Ameeshas-MacBook-Air.local>
2022-08-07 23:57:45 +0530

    code

---
+++ java/theory/29 jul/radius.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+
+class radius{
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the radius");
+        int r = sc.nextInt();
+        float pi = 3.14;
+        float area = pi*r*r;
+
+        System.out.println("Area of the circle"+area);
+    }
+}
---
+++ java/theory/29jul.java
@@ -0,0 +1,9 @@
+// find the minimum and maximum in an array entered by a user
+
+import java.util.* ;
+
+class 29jul {
+    public static void main(String args[]){
+        int arr a = []
+    }
+}
